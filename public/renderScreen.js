17a076de412f027ed22c5ed4f19b829e82520560
Rafael Morais da Cunha <rafaelmoraisdacunha@gmail.com>
2020-11-15 12:14:45 -0300

    added eslint configuration

--- public/renderScreen.js
+++ public/renderScreen.js
@@ -4,7 +4,6 @@ const playerColor = 'gray';
 const candyColor = 'blue';
 
 export default function renderScreen(screen, game, currentPlayerId) {
-
     const context = screen.getContext('2d');
 
     context.fillStyle = backgroundColor;
@@ -13,8 +12,7 @@ export default function renderScreen(screen, game, currentPlayerId) {
     for (const playerId in game.state.players) {
         if (playerId != currentPlayerId) {
             context.fillStyle = playerColor;
-        }
-        else {
+        } else {
             context.fillStyle = CurrentPlayerColor;
         }
         const player = game.state.players[playerId];
